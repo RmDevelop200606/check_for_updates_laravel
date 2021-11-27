65e53dbb40a989b922b8fbe4967295643646bbba
kkenji-rmg <79568046+kkenji-rmg@users.noreply.github.com>
2021-11-27 19:57:06 +0900

    更新

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -54,7 +54,7 @@
                     </x-nav-link>
                 </div>
                 
-                @if(Auth::user()->is_admin == 1)
+                @if (Auth::user()->is_admin == 1)
                     <!-- Python設定 -->
                     <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                         <x-nav-link :href="route('pysetting.index')" :active="request()->routeIs('pysetting.index')">
