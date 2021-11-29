4ff151ea0f40fcecbc295348bf5b1653f2b9567d
rm-miyatake <89385120+rm-miyatake@users.noreply.github.com>
2021-11-29 16:44:26 +0900

    Revert "Revert "ブログあり、更新あり表示""
    This reverts commit 84cd3ecc167362dc9e5aacf8ba64bba44b40e0aa.

--- app/Http/Controllers/HasBlogController.php
+++ app/Http/Controllers/HasBlogController.php
@@ -28,68 +28,28 @@ class HasBlogController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
+     * 更新ありのユーザーのみ表示
+     * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
-        //
-    }
+    public function updated(){
+        // リクエストに応じたクエリを発行
+        $customers = Customer::with('long_diff')
+                ->where('blog_flg', 1)
+                ->where('active_flg', 1)
+                ->where('del_flg', 0)
+                ->whereHas('long_diff', function($query){
+                    $query->where('difference_flg', 1)
+                        ->groupBy('customer_id');
+                })
+                ->sortable()
+                ->paginate(100);
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Customer  $customer
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Customer $customer)
-    {
-        //
-    }
+        // dd($customers->long_diff->max('time_stamp_dif_long'));
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Customer  $customer
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Customer $customer)
-    {
-        //
-    }
+        // dd($customers);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Customer  $customer
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Customer $customer)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Customer  $customer
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Customer $customer)
-    {
-        //
+        return view('hasblog-updated')->with('customers', $customers);
     }
 }
