a5527c32a66757d240399f1d2abf84c4b3dc05ba
kkenji-rmg <79568046+kkenji-rmg@users.noreply.github.com>
2021-12-13 14:06:47 +0900

    Update test_copy.py

--- app/Http/Controllers/python/test_copy.py
+++ app/Http/Controllers/python/test_copy.py
@@ -1,7 +1,3 @@
-from myfunction import openfile_to_diff
-from myfunction import checkDif_createDifFile
-from myfunction import mail_print
-
 import DBconfig
 db = DBconfig.functionDBconfig()
 mycursor = db.cursor()
@@ -10,26 +6,13 @@ import pandas.io.sql as pdsql
 # =================================================
 # ====▼▼▼▼▼▼▼▼====       関数      ====▼▼▼▼▼▼▼▼====
 # =================================================
-from myfunction import create_htmlfile
-from myfunction import removeFile
-from myfunction import checkExistInBetTabel
-from myfunction import changePathRelateiveToDirect
-from myfunction import checkDif_createDifFile
-from myfunction import tryBeautifulSoup
-from myfunction import get_linkurl
-from myfunction import mail_print
-
 
 from os import path
-import datetime
+
 import sql_sentence
 import DBconfig
-import time
-import os
 
 
-# if "favorite" in path.dirname(__file__) + "/acquired_data/20202/html/":
-#     print("true")
 
 dfDiffernceShortData = pdsql.read_sql(sql_sentence.difference_shortterm_select, db)
 
